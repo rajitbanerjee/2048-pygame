4413460a4c5ebdb20de4fa19c8b43cca32ae9673
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2019-08-18 16:54:06 +0530

    game logic

---
+++ logic.py
@@ -0,0 +1,136 @@
+import random
+
+
+def move(direction, board):
+    if direction == "w":
+        return moveUp(board)
+    if direction == "s":
+        return moveDown(board)
+    if direction == "a":
+        return moveLeft(board)
+    if direction == "d":
+        return moveRight(board)
+
+
+def newGame():
+    board = [[0] * 4 for _ in range(4)]
+    return board
+
+
+def checkGameStatus(board):
+    flat_board = [cell for row in board for cell in row]
+    if 256 in flat_board:
+        return "WIN"
+
+    for i in range(4):
+        for j in range(4):
+            if j != 3 and board[i][j] == board[i][j+1] or \
+                    i != 3 and board[i][j] == board[i + 1][j]:
+                return "PLAY"
+
+    if 0 not in flat_board:
+        return "LOSE"
+    else:
+        return "PLAY"
+
+
+def fillTwoOrFour(board, iter=1):
+    for _ in range(iter):
+        a = random.randint(0, 3)
+        b = random.randint(0, 3)
+        while(board[a][b] != 0):
+            a = random.randint(0, 3)
+            b = random.randint(0, 3)
+
+        if sum([cell for row in board for cell in row]) in (0, 2):
+            board[a][b] = 2
+        else:
+            board[a][b] = random.choice([2, 4])
+    return board
+
+
+def moveLeft(board):
+    # initial shift
+    shiftLeft(board)
+
+    # merge cells
+    for i in range(4):
+        for j in range(3):
+            if board[i][j] == board[i][j + 1] and board[i][j] != 0:
+                board[i][j] *= 2
+                board[i][j + 1] = 0
+                j = 0
+
+    # final shift
+    shiftLeft(board)
+    return board
+
+
+def moveUp(board):
+    board = rotateLeft(board)
+    board = moveLeft(board)
+    board = rotateRight(board)
+    return board
+
+
+def moveRight(board):
+    # initial shift
+    shiftRight(board)
+
+    # merge cells
+    for i in range(4):
+        for j in range(3, 0, -1):
+            if board[i][j] == board[i][j - 1] and board[i][j] != 0:
+                board[i][j] *= 2
+                board[i][j - 1] = 0
+                j = 0
+
+    # final shift
+    shiftRight(board)
+
+    return board
+
+
+def moveDown(board):
+    board = rotateLeft(board)
+    board = moveLeft(board)
+    shiftRight(board)
+    board = rotateRight(board)
+    return board
+
+
+def shiftLeft(board):
+    # remove 0's in between numbers
+    for i in range(4):
+        nums, count = [], 0
+        for j in range(4):
+            if board[i][j] != 0:
+                nums.append(board[i][j])
+                count += 1
+        board[i] = nums
+        board[i].extend([0] * (4 - count))
+
+
+def shiftRight(board):
+    # remove 0's in between numbers
+    for i in range(4):
+        nums, count = [], 0
+        for j in range(4):
+            if board[i][j] != 0:
+                nums.append(board[i][j])
+                count += 1
+        board[i] = [0] * (4 - count)
+        board[i].extend(nums)
+
+
+def rotateLeft(board):
+    # 90 degree counter clockwise rotation
+    b = [[board[j][i] for j in range(4)] for i in range(3, -1, -1)]
+    return b
+
+
+def rotateRight(board):
+    # 270 degree counter clockwise rotation
+    b = rotateLeft(board)
+    b = rotateLeft(b)
+    return rotateLeft(b)
